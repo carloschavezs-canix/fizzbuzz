6ff5ea5160e1aca73b14f77f53b9d8b7d1755421
Carlitos <carlos.chavez.s@ucb.edu.bo>
2026-08-28 21:46:29 -0400

    test: manejar numeros diferentes

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,5 @@
 function fizzbuzz(numero) {
-    return "1";
+    return numero;
 }
 
 export default fizzbuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -3,7 +3,11 @@ import fizzbuzz from "./fizzbuzz.js";
 describe("FizzBuzz", () => {
 
     test("debería devolver 1 si recibe 1", () => {
-        expect(fizzbuzz(1)).toEqual("1");
+        expect(fizzbuzz(1)).toEqual(1);
+    });
+    
+    test("debería devolver el mismo número si recibe otro número", () => {
+        expect(fizzbuzz(2)).toEqual(2);
     });
 
 });
